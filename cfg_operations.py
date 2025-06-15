cde0dd5168261b8b875d2cc97576b0519326818c
DELARAAM <delaraamroohani@gmail.com>
2025-06-15 13:09:43 +0330

    Remove null productions function done

--- cfg_operations.py
+++ cfg_operations.py
@@ -1,8 +1,10 @@
+import copy
+
 class CFG:
     def __init__(self):
         self.variables = set()
         self.terminals = set()
-        self.productions = {} # str -> list(str)
+        self.productions = {} # str -> set(str)
         self.start = ''
 
     def __str__(self):
@@ -14,9 +16,9 @@ class CFG:
         s += self.start
         s += "\n# Productions\n"
         
-        for (lhs, rhs) in self.productions.items():
-            for r in rhs:
-                s += f"{lhs} -> {r}\n"
+        for (lhs, rhss) in self.productions.items():
+            for rhs in rhss:
+                s += f"{lhs} -> {rhs}\n"
 
         return s
     
@@ -24,29 +26,59 @@ class CFG:
         if lhs not in self.variables:
             print(f"Production {lhs} -> {rhs} does not follow context-free grammar rules.")
             return
-        self.productions.setdefault(lhs, []).append(rhs)
+        self.productions.setdefault(lhs, set()).add(rhs)
+
+    def remove_production(self, lhs, rhs):
+        self.productions.get(lhs).remove(rhs)
 
 def get_nullables(cfg):
     old_nullables = set()
     new_nullables = set()
-    for lhs, rhs in cfg.productions.items():
-        for r in rhs:
-            if r == "Ɛ":
-                print(f"rhs is epsilon: {lhs} -> {r}")
+    for lhs, rhss in cfg.productions.items():
+        for rhs in rhss:
+            if rhs == "Ɛ":
+                print(f"rhs is epsilon: {lhs} -> {rhs}")
                 new_nullables.add(lhs)
 
     while old_nullables != new_nullables:
         old_nullables = new_nullables
-        for lhs, rhs in cfg.productions.items():
-            for r in rhs:
+        for lhs, rhss in cfg.productions.items():
+            for rhs in rhss:
                 flag = False
-                for c in r:   # parse string
+                for c in rhs:   # parse string
                     if c not in old_nullables:
                         flag = True
                     if flag == False:
                         new_nullables.add(lhs)
         
     return old_nullables
+
+def remove_null_productions(old_cfg):
+    cfg = copy.deepcopy(old_cfg)
+    nullables = get_nullables(cfg)
+
+    for lhs, rhss in cfg.productions.items():
+        rhss_frozen = frozenset(rhss)
+        for rhs in rhss_frozen:
+            if rhs == "Ɛ":
+                cfg.remove_production(lhs, rhs)
+
+    old_prod = {}
+    new_prod = copy.deepcopy(cfg.productions)
+
+    while old_prod != new_prod:
+        old_prod = copy.deepcopy(new_prod)
+        new_prod = {}
+        for lhs, rhss in old_prod.items():
+            for rhs in rhss:
+                if len(rhs) > 1:
+                    for c in rhs:
+                        if c in nullables:
+                            split = rhs.split(c, 1)
+                            cfg.add_production(lhs, split[0] + split[1])
+                            new_prod.setdefault(lhs, set()).add(split[0] + split[1])
+    return cfg
+
 
 
 if __name__ == "__main__":
@@ -62,4 +94,4 @@ if __name__ == "__main__":
     cfg.add_production('C', "Ɛ")
     cfg.add_production('D', "d")
 
-    print(get_nullables(cfg))
+    print(remove_null_productions(cfg))
